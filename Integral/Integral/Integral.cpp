64b36585c83800c18083e3884dbfa2d980e6db89
Rachel-fairy <2608578933@qq.com>
2020-07-28 09:24:31 +0800

    my

--- Integral/Integral/Integral.cpp
+++ Integral/Integral/Integral.cpp
@@ -1,20 +1,19 @@
 #include<stdio.h>
 int main() {
-	float Integral(int,int);
-	float(*p)(int,int);
-	int a, b;
-	float c;
+	float Integral(float,float);
+	float(*p)(float, float);
+	float a, b, c;
 	p = Integral;
 	printf("被积函数为:f(x)=x^2+2x+1");
 	printf("\n请输入上限a=");
-	scanf_s("%d",&a);
+	scanf_s("%f",&a);
 	printf("\n请输入下限b=");
-	scanf_s("%d",&b);
+	scanf_s("%f",&b);
 	c = (*p)(a,b);
 	printf("结果为:%5.2f",c);
 	return 0;
 }
-float Integral(int x,int y) {
+float Integral(float x,float y) {
 	float z;
 	z = (x * x * x / 3 + x * x + x) - (y*y*y/3+y*y+y);
 	return(z);
